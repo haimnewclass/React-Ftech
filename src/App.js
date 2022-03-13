5966d02ca17c3d918e53250fed593254d998e628
haimnewclass <56435506+haimnewclass@users.noreply.github.com>
2022-03-13 21:47:01 +0200

    !

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
+
+
+// https://jsonplaceholder.typicode.com/
 function App() {
+  
+let [val,setVal] = useState();
+let [val2,setVal2] = useState("start");
+let [val3,setVal3] = useState("start");
+//let val3="23";
+const load=()=>{
+
+let url="https://jsonplaceholder.typicode.com/todos";
+fetch(url)
+.then(res => res.json())
+.then(
+  (result) => {
+    setVal({
+      isLoaded: true,
+      items: result
+    });
+
+    console.log("****");
+  },
+  // Note: it's important to handle errors here
+  // instead of a catch() block so that we don't swallow
+  // exceptions from actual bugs in components.
+  (error) => {
+   setVal({
+     isLoaded: true,
+     error:error
+   });
+  }
+)
+}
+
+useEffect(load,[val3])
+//load();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     start
+     <div onClick={()=>{
+
+       let url="https://jsonplaceholder.typicode.com/todos";
+       fetch(url)
+       .then(res => res.json())
+       .then(
+         (result) => {
+           setVal({
+             isLoaded: true,
+             items: result
+           });
+
+           console.log("****");
+         },
+         // Note: it's important to handle errors here
+         // instead of a catch() block so that we don't swallow
+         // exceptions from actual bugs in components.
+         (error) => {
+          setVal({
+            isLoaded: true,
+            error:error
+          });
+         }
+       )
+
+
+
+     }
+    }>click</div>
+
+<div>{val2}</div>
+    <input onChange={(i)=>{
+      setVal2(i.target.value);
+      if(i.target.value==="*") {
+        console.log('onChange')
+        setVal3("1")};
+    }} />
+
+    {
+    val? val.items.map((i)=>{
+      return (<div>{i.title}</div>);
+    }):""}
     </div>
   );
 }
